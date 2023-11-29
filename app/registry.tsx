b7036a56014c4dc2d2a80fef4737d0b2db0cb2dd
DESKTOP-5SMQO9E\izich <ivan.name Ivan>
2023-11-28 23:09:22 -0300

    fix/resolved-lazy-styled-components-load

--- app/registry.tsx
+++ app/registry.tsx
@@ -1,23 +1,34 @@
-'use client'
- 
-import React, { useState } from 'react'
-import { useServerInsertedHTML } from 'next/navigation'
-import { StyleRegistry, createStyleRegistry } from 'styled-jsx'
- 
-export default function StyledJsxRegistry({
+'use client';
+
+import React, { useState } from 'react';
+import { useServerInsertedHTML } from 'next/navigation';
+import {
+  ServerStyleSheet,
+  StyleSheetManager,
+} from 'styled-components';
+
+export default function StyledComponentsRegistry({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   // Only create stylesheet once with lazy initial state
   // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
-  const [jsxStyleRegistry] = useState(() => createStyleRegistry())
- 
+  const [styledComponentsStyleSheet] = useState(
+    () => new ServerStyleSheet()
+  );
+
   useServerInsertedHTML(() => {
-    const styles = jsxStyleRegistry.styles()
-    jsxStyleRegistry.flush()
-    return <>{styles}</>
-  })
- 
-  return <StyleRegistry registry={jsxStyleRegistry}>{children}</StyleRegistry>
+    const styles = styledComponentsStyleSheet.getStyleElement();
+    styledComponentsStyleSheet.instance.clearTag();
+    return <>{styles}</>;
+  });
+
+  if (typeof window !== 'undefined') return <>{children}</>;
+
+  return (
+    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+      {children}
+    </StyleSheetManager>
+  );
 }
